dca43e3debf2dad47f926a21ee9a01513ea30b33
luozikang <18170603701@163.com>
2020-09-03 19:34:12 +0800

    add decode ,change pom

--- auth/authentication-client/src/main/java/com/springboot/cloud/auth/client/config/FeignOkHttpConfig.java
+++ auth/authentication-client/src/main/java/com/springboot/cloud/auth/client/config/FeignOkHttpConfig.java
@@ -1,12 +1,22 @@
 package com.springboot.cloud.auth.client.config;
 
 import feign.Feign;
+import feign.codec.Decoder;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @AutoConfigureBefore(FeignAutoConfiguration.class)
@@ -35,27 +45,27 @@ public class FeignOkHttpConfig {
 	}
 
 
-//	@Bean
-//	public Decoder feignDecoder() {
-//		return new ResponseEntityDecoder(new SpringDecoder(feignHttpMessageConverter()));
-//	}
-//
-//	public ObjectFactory<HttpMessageConverters> feignHttpMessageConverter() {
-//		final HttpMessageConverters httpMessageConverters = new HttpMessageConverters(new GateWayMappingJackson2HttpMessageConverter());
-//		return new ObjectFactory<HttpMessageConverters>() {
-//			@Override
-//			public HttpMessageConverters getObject() throws BeansException {
-//				return httpMessageConverters;
-//			}
-//		};
-//	}
-//
-//	public class GateWayMappingJackson2HttpMessageConverter extends MappingJackson2HttpMessageConverter {
-//		GateWayMappingJackson2HttpMessageConverter() {
-//			List<MediaType> mediaTypes = new ArrayList<>();
-//			mediaTypes.add(MediaType.valueOf(MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"));
-//			setSupportedMediaTypes(mediaTypes);
-//		}
-//	}
+	@Bean
+	public Decoder feignDecoder() {
+		return new ResponseEntityDecoder(new SpringDecoder(feignHttpMessageConverter()));
+	}
+
+	public ObjectFactory<HttpMessageConverters> feignHttpMessageConverter() {
+		final HttpMessageConverters httpMessageConverters = new HttpMessageConverters(new GateWayMappingJackson2HttpMessageConverter());
+		return new ObjectFactory<HttpMessageConverters>() {
+			@Override
+			public HttpMessageConverters getObject() throws BeansException {
+				return httpMessageConverters;
+			}
+		};
+	}
+
+	public class GateWayMappingJackson2HttpMessageConverter extends MappingJackson2HttpMessageConverter {
+		GateWayMappingJackson2HttpMessageConverter() {
+			List<MediaType> mediaTypes = new ArrayList<>();
+			mediaTypes.add(MediaType.valueOf(MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"));
+			setSupportedMediaTypes(mediaTypes);
+		}
+	}
 
 }
